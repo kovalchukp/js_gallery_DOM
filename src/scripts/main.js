1c8f7ce9a4c4f30296c95998c394eab7df481a5e
kovalchukp <you@example.comp.kovalchuk@icloud.com>
2025-09-28 21:52:02 -0600

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,28 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const largeImage = document.getElementById('largeImg');
+  const galleries = document.querySelectorAll('.gallery__list');
+
+  galleries.forEach((ul) => {
+    ul.addEventListener('click', (e) => {
+      e.preventDefault();
+
+      const img = e.target.closest('a');
+
+      if (!largeImage || !ul.contains(img)) {
+        return;
+      }
+
+      if (!img) {
+        return;
+      }
+
+      const href = new URL(img.getAttribute('href'), window.location).href;
+      const title = img.getAttribute('title');
+
+      largeImage.src = href;
+      largeImage.alt = title || '';
+    });
+  });
+});
